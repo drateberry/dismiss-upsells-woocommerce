a61ce610aac20fba7312497eff89178d45f53b60
Drate Berry <drate@osostudio.com>
2019-04-04 19:42:25 -0500

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Dismiss Upsells for WooCommerce ===
+Contributors: drateberry
+Donate link: https://starterwp.com/donate
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+Tags: Remove upsells, WooCommerce marketplace
+Requires at least: 4.9
+Tested up to: 5.1.1
+Stable tag: 1.0
+Requires PHP: 5.2.4
+
+Remove the WooCommerce Marketplace upsells introduced in version 3.6
+
+== Description ==
+
+### Dismiss Suggestions for WooCommerce
+This plugin removes the upsells for Automattic's WooCommerce plugin from your WordPress website. In Version 3.6 of WooCommerce, links to the WooCommerce Marketplace (operated by Automattic) are included on product admin pages.
+
+Remove Upsells for WooCommerce will permanently disable marketplace suggestions instead of the native requirement to dismiss the upsell notifications daily for 5 days each month.
+
+== Installation ==
+
+=== Manually ===
+
+1. Upload the `starterwp-dismiss-upsells-woocommerce` folder to the `/wp-content/plugins/` directory
+1. Activate the Dismiss Upsells for WooCommerce plugin through the 'Plugins' menu in WordPress
+1. The plugin will prevent WooCommerce from loading marketplace suggestions outside of the WooCommerce Extensions page
---
+++ starterwp-dismiss-upsells-woocommerce.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Plugin Name: Dismiss Suggestions for WooCommerce
+Plugin URI: https://starterwp.com
+Description: Remove WooCommerce Marketplace suggestions from product pages.
+Version: 1.0.0
+Author: Starter WP
+Author URI: https://starterwp.com
+Text Domain: starterwp-dismiss-upsells-woocommerce
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+if ( ! function_exists( 'add_filter' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
+//Add update library
+if ( ! defined( 'YOUR_REPO_URL' ) ) {
+	define( 'YOUR_REPO_URL', 'https://starterwp.com/update/' );
+}
+if ( ! defined( 'YOUR_PLUGIN_UNIQUE' ) ) {
+	define( 'YOUR_PLUGIN_UNIQUE', 'gqaxsrb5SdDlNVHP' );
+}
+if ( ! defined( 'YOUR_PLUGIN_VER' ) ) {
+	define( 'YOUR_PLUGIN_VER', '1.0.0' );
+}
+
+add_action( 'plugins_loaded', 'rkv_load_updater' );
+
+function rkv_load_updater() {
+
+	if ( ! class_exists( 'RKV_Remote_Updater' ) ) {
+		include( 'lib/RKV_Remote_Updater.php' );
+	}
+}
+
+add_action( 'admin_init', 'rkv_remote_update' );
+
+function rkv_remote_update() {
+
+	// ensure the class exists before running
+	if ( ! class_exists( 'RKV_Remote_Updater' ) ) {
+		return;
+	}
+
+	$updater = new RKV_Remote_Updater( YOUR_REPO_URL, __FILE__, array(
+		'unique'    => YOUR_PLUGIN_UNIQUE,
+		'version'   => YOUR_PLUGIN_VER,
+		)
+	);
+}
+
+//Remove WooCommerce Marketplace suggestions
+add_filter('woocommerce_allow_marketplace_suggestions', '__return_false');
+
+?>
